f4442245998fdf8e4431d7a63a252beffbf7a104
Alberto Miranda <alberto.miranda@opendeusto.es>
2019-01-07 16:59:41 +0100

    Created request to SpotifyAPI
    Playlist List enable

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
 const inquirer  = require('./lib/inquirer');
+const spoty  = require('./lib/spoty');
 
 
-clear();
+console.clear();
 
 function reset (op) {
 
-	clear();
+	console.clear();
 	banner();			
 	
 }
@@ -20,13 +21,13 @@ function banner () {
 		    		figlet.textSync('Spotify Multi Tool', { horizontalLayout: 'full' })
 		  	)
 	);
-	console.log('Developed by Alberto Miranda [albertomgv.com]');
-	console.log('v1.0.0');
+	console.log(chalk.yellow('Developed by Alberto Miranda [albertomgv.com]'));
+	console.log(chalk.yellow('v1.0.0'));
 	console.log();
 }
 
 
-const name = async () => {
+const start = async () => {
 	const user = await inquirer.askName();
   	reset();
   	console.log('Welcome '+user.name+'!');
@@ -43,8 +44,13 @@ function options() {
 const option = async () => {
   	const option = await inquirer.askOption();
   	reset();
-  	if (option.option[1]==null) {
-  		console.log('You`ve selected ['+option.option[0]+']')
+  	if (option.option[0]==null) {
+  		reset();
+  		console.log(chalk.red('* Select only one option!'));
+  		options();
+
+  	} else if (option.option[1]==null) {
+  	
   		switch (option.option[0].charAt(0)) {
 			case '1':
 	    		option1();
@@ -67,16 +73,44 @@ const option = async () => {
 }
 
 const option1 = async () => {
-		console.log('Not defined yet sorry');
+		console.log(chalk.green('***** Playlist Copier *****'));
+		console.log("");
+		console.log("From:");
+		const pl1 = await inquirer.askPlaylist();
+		console.log("To:");
+		const pl2 = await inquirer.askPlaylist();
+
+	  	spoty.playCopier(pl1.playlist,pl2.playlist);
+	  	
 }
 
 const option2 = async () => {  	
-		console.log('Not defined yet sorry');
+		console.log(chalk.green('***** Playlist List *****'));
+		console.log("");
+		const pl = await inquirer.askPlaylist();
+		reset();
+		console.log(chalk.green('***** Playlist Songs *****'));
+		console.log('');
+	  	spoty.getSongs(pl.playlist);
+	  	back();
 }
 
 const option3 = async () => {
-  		console.log('Not defined yet sorry');
+  		console.log(chalk.red('Not defined yet sorry'));
+}
+
+const back = async () => {
+  		const back = await inquirer.back();
+  		if (back.answer=='y' || back.answer=='Y') {
+  			reset();
+  			option();
+  		} else {
+  			console.log(chalk.green("See you soon <3"))
+  		}
+
+  		
+	  	
 }
 
 banner();
-name();
+start();
